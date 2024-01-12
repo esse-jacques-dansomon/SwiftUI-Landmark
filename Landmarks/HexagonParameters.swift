d520916a8cac5c6c7b390eb6b60f20ba006217bb
esse.jacques.co <essedansomon@gmail.com>
2024-01-12 11:11:08 +0000

    animation

---
+++ Landmarks/HexagonParameters.swift
@@ -0,0 +1,9 @@
+//
+//  HexagonParameters.swift
+//  Landmarks
+//
+//  Created by Esse Jacques  on 11/01/2024.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import Foundation
---
+++ Landmarks/Model/Hike.swift
@@ -0,0 +1,9 @@
+//
+//  Hike.swift
+//  Landmarks
+//
+//  Created by Esse Jacques  on 12/01/2024.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import Foundation
